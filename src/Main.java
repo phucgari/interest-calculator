9daf0bb08129cabfaea2fe30f13ebce9d0e274e6
phucgari <phucgari@gmail.com>
2023-01-06 10:51:47 +0700

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the initial amount: ");
+        double money=scanner.nextDouble();
+        System.out.println("Enter number of months: ");
+        double month=scanner.nextDouble();
+        System.out.println("Enter annual interest rate in percentage: ");
+        double interest=scanner.nextDouble();
+        double total= money*(interest/100)/12*month;
+        System.out.println("Total of interest: "+total);
     }
 }
